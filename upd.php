9cf7b17f286968e84a42ac4724633e58526336fa
Roskoliy Kirill(Ro$k0) <roskoliy.kirill@gmail.com>
2011-10-24 15:42:04 +0300

    Initial commit

---
+++ upd.php
@@ -0,0 +1,16 @@
+<?php
+
+$user = 'TRIAL-53698007';
+$pass = 'xerbu3fhef';
+
+exec('wget http://um10.eset.com/eset_upd/v5/update.ver 2>&1', $output, $return);
+
+
+
+
+
+var_dump($output);
+
+//var_dump(get_defined_vars());
+?>
+
