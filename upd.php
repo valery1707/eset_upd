125e53c9b767e8000b4b850dce49955ab3ec98cf
Roskoliy Kirill(Ro$k0) <roskoliy.kirill@gmail.com>
2011-10-25 00:52:49 +0300

    dsf

--- upd.php
+++ upd.php
@@ -7,8 +7,22 @@ $tmp_path = '/tmp/eset_upd';
 $web_path = '/var/test/eset_upd';
 $srv = 'http://um10.eset.com/eset_upd/v5/';
 
-//exec('wget http://um10.eset.com/eset_upd/v5/update.ver 2>&1', $output, $return);
+exec('wget http://um10.eset.com/eset_upd/v5/update.ver 2>&1', $output, $return);
 
+if(!file_exists($tmp_path)){
+  $path = explode('/', $tmp_path);
+  if(!file_exists($path[0])){
+    exec('mkdir ' . $path);
+  }elseif (!file_exists($path[0])){
+    exec('mkdir ' . $path[0] . '/' . $path[1]);
+  }
+};
+
+$h = fopen($tmp_path . '/log', 'w+');
+foreach ($output as $str){
+  fwrite($h, $str . '\n');
+}
+fwrite($h, '\n' . $return);
 $arr = parse_ini_file('update.ver4', TRUE, INI_SCANNER_RAW);
 
 
