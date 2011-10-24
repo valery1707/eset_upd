11a4575fca8046045615eda307cc6c34a1847de3
Roskoliy Kirill(Ro$k0) <roskoliy.kirill@gmail.com>
2011-10-25 00:04:41 +0300

    modified:   upd.php

--- upd.php
+++ upd.php
@@ -3,13 +3,18 @@
 $user = 'TRIAL-53698007';
 $pass = 'xerbu3fhef';
 
-exec('wget http://um10.eset.com/eset_upd/v5/update.ver 2>&1', $output, $return);
+$tmp_path = '/tmp/eset_upd';
+$web_path = '/var/test/eset_upd';
+$srv = 'http://um10.eset.com/eset_upd/v5/';
+
+//exec('wget http://um10.eset.com/eset_upd/v5/update.ver 2>&1', $output, $return);
+
+$arr = parse_ini_file('update.ver4', TRUE, INI_SCANNER_RAW);
 
 
 
 
-
-var_dump($output);
+echo($arr['HOSTS']['Other']);
 
 //var_dump(get_defined_vars());
 ?>
