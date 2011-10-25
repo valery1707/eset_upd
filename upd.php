6920c3aee035b49b32e83c2f71e943a6f741bd71
Roskoliy Kirill(Ro$k0) <roskoliy.kirill@gmail.com>
2011-10-25 15:41:36 +0300

    added chkdir and download funcs

--- upd.php
+++ upd.php
@@ -3,32 +3,57 @@
 $user = 'TRIAL-53698007';
 $pass = 'xerbu3fhef';
 
-$tmp_path = '/tmp/eset_upd';
-$web_path = '/var/test/eset_upd';
+$tmp_path = '/tmp/test/eset_upd/tmp/';
+$web_path = '/tmp/test/eset_upd/www/';
 $srv = 'http://um10.eset.com/eset_upd/v5/';
+$srv = 'update.eset.com';
+$tail = 'eset_upd';
+$db = '5';
 
-exec('wget http://um10.eset.com/eset_upd/v5/update.ver 2>&1', $output, $return);
 
-if(!file_exists($tmp_path)){
-  $path = explode('/', $tmp_path);
-  if(!file_exists($path[0])){
-    exec('mkdir ' . $path);
-  }elseif (!file_exists($path[0])){
-    exec('mkdir ' . $path[0] . '/' . $path[1]);
-  }
-};
 
+
+$full_tail = '/' . $tail . '/v' . $db;
+$proto = 'http://';
+#exec('wget http://um10.eset.com/eset_upd/v5/update.ver 2>&1', $output, $return);
+function download($file, $dir){
+global $proto, $user, $pass, $srv, $full_tail; 
+  exec('wget ' . $proto .$user . ':' . $pass . '@' . $srv . $full_tail . '/' . $file .' -O ' . $dir. '/' . $file . ' -a '.  $dir . '/dl.log');
+}
+
+download('update.ver', $tmp_path);
+
+
+//var_dump($host);
+function chkpath($path){
+ $out = array();
+ $ret = array();
+ if(!file_exists($path)) exec('mkdir --parents ' . $path, $out, $ret);
+//var_dump($out, 'Out');
+//var_dump($ret, 'Ret');
+//$ret === 0 ? return 'OK' : return 'Error'
+}
+
+/**
+ *  Провверка существования пути
+ *
+//chkpath($tmp_path);
+
+
+
+/*
 $h = fopen($tmp_path . '/log', 'w+');
 foreach ($output as $str){
   fwrite($h, $str . '\n');
 }
 fwrite($h, '\n' . $return);
+
 $arr = parse_ini_file('update.ver4', TRUE, INI_SCANNER_RAW);
+*/
 
 
 
-
-echo($arr['HOSTS']['Other']);
+//echo($arr['HOSTS']['Other']);
 
 //var_dump(get_defined_vars());
 ?>
