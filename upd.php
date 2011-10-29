397b6fd3d67b89c7104a52296478bff5682a3351
Roskoliy Kirill(Ro$k0) <roskoliy.kirill@gmail.com>
2011-10-30 00:40:41 +0300

    Allmoust done

--- upd.php
+++ upd.php
@@ -8,20 +8,37 @@ $web_path = '/tmp/test/eset_upd/www/';
 $srv = 'http://um10.eset.com/eset_upd/v5/';
 $srv = 'update.eset.com';
 $tail = 'eset_upd';
-$db = '5';
+$db = '4';
 
+$unrar = '/usr/bin/unrar';
+$ru = 1049;
+$en = 1033;
 
 
 
 $full_tail = '/' . $tail . '/v' . $db;
 $proto = 'http://';
 #exec('wget http://um10.eset.com/eset_upd/v5/update.ver 2>&1', $output, $return);
-function download($file, $dir){
-global $proto, $user, $pass, $srv, $full_tail; 
-  exec('wget ' . $proto .$user . ':' . $pass . '@' . $srv . $full_tail . '/' . $file .' -O ' . $dir. '/' . $file . ' -a '.  $dir . '/dl.log');
-}
 
-download('update.ver', $tmp_path);
+
+
+//download($full_tail . 'update.ver', $tmp_path);
+//rename($tmp_path . 'update.ver', $tmp_path . 'update.rar');
+exec($unrar . ' x -o+ - ' . $tmp_path . 'update.rar ' . $tmp_path , $out, $ret);
+//var_dump($out, 'Out');
+//var_dump($ret, 'Ret');
+$settings = parse_ini_file($tmp_path . 'update.ver', TRUE, INI_SCANNER_RAW);
+foreach ($settings as $section){
+	//var_dump($section);
+	if(isset($section['file'])){
+		if (isset($section['language']) && $section['language'] != $ru) continue;
+		//echo 'Going to download ' .$section['file'];
+		download($section['file'], $tmp_path);
+		if( filesize($tmp_path . basename($section['file'])) != $section['size']) unlink($tmp_path . basename($section['file']));
+	}
+}
+//var_dump($settings);
+
 
 
 //var_dump($host);
@@ -32,6 +49,18 @@ function chkpath($path){
 //var_dump($out, 'Out');
 //var_dump($ret, 'Ret');
 //$ret === 0 ? return 'OK' : return 'Error'
+}
+
+
+function download($file, $dir){
+	global $proto, $user, $pass, $srv;
+	chkpath($dir);
+	//var_dump($proto . $user . ':' . $pass . '@' . $srv . '/' . $file);
+	exec('wget ' . $proto . $user . ':' . $pass . '@' . $srv . '/' . $file .' -O ' . $dir. '/' . basename($file) . ' -a '.  $dir . '/dl.log', $out, $ret);
+	//var_dump($out);
+	//var_dump($ret);
+	//exit();
+	
 }
 
 /**
