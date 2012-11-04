ffeb68e3d65e5b3f59f9c3b53120b24e2a1a5b04
valery1707 <valery1707@gmail.com>
2012-11-04 12:55:32 +0600

    Detect working dir

--- src/main/java/name/valery1707/tools/EsetUpdater.java
+++ src/main/java/name/valery1707/tools/EsetUpdater.java
@@ -1,7 +1,30 @@
 package name.valery1707.tools;
 
+import java.io.File;
+
 public class EsetUpdater {
 
     public static void main(String[] args) {
+        EsetUpdater esetUpdater = new EsetUpdater();
+        esetUpdater.run();
+    }
+
+    private final File rootDir;
+
+    public EsetUpdater() {
+        rootDir = detectRootDir();
+    }
+
+    public void run() {
+        System.out.println("Running at: " + rootDir.getAbsolutePath());
+    }
+
+    private static File detectRootDir() {
+        String classPath = System.getProperty("java.class.path");
+        if (!classPath.contains(File.pathSeparator)) {
+            return new File(classPath).getParentFile().getAbsoluteFile();
+        } else {
+            return new File(System.getProperty("user.dir")).getAbsoluteFile();
+        }
     }
 }
